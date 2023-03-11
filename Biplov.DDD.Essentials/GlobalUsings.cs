f8947cecd5e9f58c39acfced2d077b6db8aca1e6
Biplov <biplov.cybercop@gmail.com>
2023-03-11 11:42:53 +0100

    fix missing namespacenamespaces fix

--- Biplov.DDD.Essentials/GlobalUsings.cs
+++ Biplov.DDD.Essentials/GlobalUsings.cs
@@ -4,3 +4,5 @@ global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Reflection;
+global using System.Text.Json.Serialization;
+global using System.Threading.Tasks;
