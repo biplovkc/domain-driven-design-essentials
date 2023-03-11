cc395ed92fe9479946d42ee68236d831aa971cb9
BiplovKC <biplov_cybercop@hotmail.com>
2023-03-11 10:13:23 +0100

    added integration event and integration event handler

---
+++ Biplov.DDD.Essentials/IIntegrationEventHandler.cs
@@ -0,0 +1,11 @@
+namespace Biplov.DDD.Essentials;
+
+public interface IIntegrationEventHandler<in TIntegrationEvent> : IIntegrationEventHandler
+    where TIntegrationEvent : IntegrationEvent
+{
+    Task Handle(TIntegrationEvent @event);
+}
+
+public interface IIntegrationEventHandler
+{
+}
